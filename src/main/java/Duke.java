faa56bb1eb8b1dbf4c0754d9f6456a0e89e0821e
Tan Wei Yang <weiyang.1996@gmail.com>
2019-08-22 03:05:55 +0800

    Implemented echo and exit for Level-1. Level-1 functionality complete

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,12 +8,25 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
         Scanner input = new Scanner(System.in);
+        Duke duke = new Duke();
+        duke.greet();
+        duke.echoLoop(input);
     }
 
-    public void dummy() {
+    public void greet() {
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+    }
+
+    public void echoLoop(Scanner input) {
+        String cmd = input.next();
+        if (cmd.equals("bye")) {
+            System.out.println("Bye. Hope to see you again soon!");
+        } else {
+            System.out.println(cmd);
+            echoLoop(input);
+        }
 
     }
 }
