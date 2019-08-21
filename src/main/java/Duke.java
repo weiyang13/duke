feed0c6dd2228fdefcd83d7bd826f129487acfe9
Tan Wei Yang <weiyang.1996@gmail.com>
2019-08-22 03:31:11 +0800

    Updated program with horizontal lines and indentation

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 
 public class Duke {
     ArrayList<String> taskList;
+    Scanner input;
 
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -11,42 +12,57 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        Scanner input = new Scanner(System.in);
         Duke duke = new Duke();
         duke.greet();
-        duke.readCommand(input);
+        duke.readCommand();
     }
 
     public Duke() {
         taskList = new ArrayList<>();
+        input = new Scanner(System.in);
     }
 
     public void greet() {
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
+        printHorizontal();
+        printLine("Hello! I'm Duke");
+        printLine("What can I do for you?");
+        printHorizontal();
     }
 
-    public void readCommand(Scanner input) {
+    public void readCommand() {
         String cmd = input.nextLine();
         if (cmd.equals("bye")) {
             bye();
         } else if (cmd.equals("list")) {
             list();
+            readCommand();
         } else {
             add(cmd);
-            readCommand(input);
+            readCommand();
         }
     }
 
     public void list() {
-
+        printHorizontal();
+        printHorizontal();
     }
 
     public void add(String task) {
-        
+        printHorizontal();
+        printHorizontal();
     }
 
     public void bye() {
-        System.out.println("Bye. Hope to see you again soon!");
+        printHorizontal();
+        printLine("Bye. Hope to see you again soon!");
+        printHorizontal();
+    }
+
+    public void printHorizontal() {
+        System.out.println("    ____________________________________________________________");
+    }
+
+    public void printLine(String output) {
+        System.out.println("     " + output);
     }
 }
