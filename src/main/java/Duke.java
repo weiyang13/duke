33e1a3bbd0502df60eb1e5c00ddc09692b79af00
Tan Wei Yang <weiyang.1996@gmail.com>
2019-08-22 03:24:06 +0800

    Restructure program with readCommand() as dispatcher

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
+    ArrayList<String> taskList;
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -11,7 +14,11 @@ public class Duke {
         Scanner input = new Scanner(System.in);
         Duke duke = new Duke();
         duke.greet();
-        duke.echoLoop(input);
+        duke.readCommand(input);
+    }
+
+    public Duke() {
+        taskList = new ArrayList<>();
     }
 
     public void greet() {
@@ -19,14 +26,27 @@ public class Duke {
         System.out.println("What can I do for you?");
     }
 
-    public void echoLoop(Scanner input) {
-        String cmd = input.next();
+    public void readCommand(Scanner input) {
+        String cmd = input.nextLine();
         if (cmd.equals("bye")) {
-            System.out.println("Bye. Hope to see you again soon!");
+            bye();
+        } else if (cmd.equals("list")) {
+            list();
         } else {
-            System.out.println(cmd);
-            echoLoop(input);
+            add(cmd);
+            readCommand(input);
         }
+    }
 
+    public void list() {
+
+    }
+
+    public void add(String task) {
+        
+    }
+
+    public void bye() {
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
